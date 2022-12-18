dbd1e73f1cdac1f5a54426fe443d2c2374f6b34c
pengyanhsha <pengyanhsha@users.noreply.github.com>
2022-12-18 17:57:21 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -3,7 +3,7 @@
 int main()
 {
 	printf("0000000000000000!!!");
-	printf("test hello world!!!");
+	printf("1111111111111111!!!");
 	printf("test hello world!!!");
 	printf("test hello world!!!");
 	printf("222222!!!");
